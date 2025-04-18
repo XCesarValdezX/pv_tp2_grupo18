858254d6eacc9a7bfd3fc4b992e5b48a25a3e8b7
XCesarValdez <ceeh.valdezi@gmail.com>
2025-04-18 18:36:48 -0300

    Ejercicio 5

---
+++ Ejercicio5.js
@@ -0,0 +1,6 @@
+let horas = 5;
+let minutos = 10;
+console.log("Horas:", horas);
+console.log("Minutos:", minutos);
+let segundos = (horas * 3600) + (minutos * 60);
+console.log("Total en segundos:", segundos);
