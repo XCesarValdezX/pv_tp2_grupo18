57435767eafb40de2e41fa49e21b943053d39d84
Nicolas Duarte <3niico0225@gmail.com>
2025-04-20 15:54:13 -0300

    agrego ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,7 @@
+const lado = 5;
+
+const area = 2 * lado;
+const perimetro = lado + lado + lado + lado;
+
+console.log("Area del cuadrado: " + area);
+console.log("Perimetro del cuadrado: " + perimetro);
